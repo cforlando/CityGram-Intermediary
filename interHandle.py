7a53996d4a38bdaa22f336797c8fb36c576458ad
Michael duPont <michael@mdupont.com>
2016-02-03 00:15:07 -0500

    Updated police data source, removed landmarks, implemented requests library, code cleaning

--- interHandle.py
+++ interHandle.py
@@ -2,73 +2,67 @@
 
 ##-- Michael duPont
 ##-- interHandler.py
-##-- Fetch data from Orlando's open data portal, format to be Citygram compliant, and print
-##-- 2015-06-09
+##-- Fetch data from a "near real time" JSON data source, format to
+##-- be Citygram compliant, and print the list of JSON objects
+##-- 2016-02-02
 
 #Usage: python interHandler.py <service>
 #Currently supports the following services:
 #  - police
-#  - landmarks
 
-import sys , json , hashlib
-if sys.version_info[0] == 2: import urllib2
-elif sys.version_info[0] == 3: from urllib.request import urlopen
-else: print("Cannot load urllib in interHandle")
+import sys, json, hashlib
+from datetime import datetime, timedelta
+from requests import get
 
-#Prints a formatted JSON string of 'aDict' and exits the script
 def output(aDict):
+	'''Prints a formatted JSON string of 'aDict' and exits the script'''
 	print(json.dumps(aDict))
 	sys.exit()
 
 #Send error message if not correct number of options
 if len(sys.argv) != 2: output({'Error':'Bad input'})
 
-#Returns a dict (from JSON) from a given URL
 def getJSON(url):
+	'''Returns a dict (from JSON) from a given URL'''
 	try:
-		if sys.version_info[0] == 2:
-			response = urllib2.urlopen(url)
-			html = response.read()
-		elif sys.version_info[0] == 3:
-			response = urlopen(url)
-			html = response.read().decode('utf-8')
-		return json.loads(html)
+		return json.loads(get(url).text.strip())
 	except:
 		output({'Error':'Data Fetch Error'})
 
-#Service parent class definition
 class service:
+	'''Service parent class definition'''
 	id = ''
 	geojson = {}
 	
-	#Class init where propList is the original object library
 	def __init__(self , propList):
+		'''Class init where propList is the original object library'''
 		self.properties = propList
 	
-	#Returns True if all given keys are in properties
 	def checkForKeys(self , keyList):
+		'''Returns True if all given keys are in properties'''
 		for item in keyList:
 			if item not in self.properties: return False
-			elif item in ['','NULL']: return False
+			elif self.properties[item] in ['','NULL']: return False
 		return True
 	
 	#The next two require child-specific implementation
-	#Returns a string fully describing the alert/event
 	def makeTitle(self):
+		'''Returns a string fully describing the alert/event'''
 		return ''
 	
-	#Returns a Citygram-compliant geometry dictionary
 	def makeGeoJSON(self):
+		'''Returns a Citygram-compliant geometry dictionary'''
 		return {}
 	
-	#Set the object id to be equal to the SHA-1 hex value of a string
 	def setID(self , aString):
+		'''Set the object id to be equal to the SHA-1 hex value of a string'''
 		hasher = hashlib.sha1()
-		hasher.update(''.join(i for i in aString if ord(i)<128)) #Removes non-ascii chars
+		#Remove non-ascii chars
+		hasher.update(''.join(i for i in aString if ord(i)<128))
 		self.id = hasher.hexdigest()
 	
-	#Returns True if object passes all validation tests
 	def validate(self):
+		'''Returns True if object passes all validation tests'''
 		try:
 			assert('title' in self.properties)  #A title has been set in the properties dict
 			assert(type(self.id) == str)        #The id is a string
@@ -78,37 +72,59 @@ class service:
 			return True
 		except: return False
 	
-	#Formats and sets required data fields and runs verification test. Returns True if successful, else False
 	def update(self):
-		title = self.makeTitle()
-		self.properties['title'] = title
-		self.setID(title)
-		self.geojson = self.makeGeoJSON()
-		return self.validate()
+		'''Formats and sets required data fields and runs verification
+		test. Returns True if successful, else False'''
+		try:
+			title = self.makeTitle()
+			self.properties['title'] = title
+			self.setID(title)
+			self.geojson = self.makeGeoJSON()
+			return self.validate()
+		except:
+			return False
 	
-	#Returns a Citygram-compliant version of the original object
 	def export(self):
-		return {'id': self.id , 'type': 'Feature' , 'geometry': self.geojson , 'properties': self.properties}
+		'''Returns a Citygram-compliant version of the original object'''
+		return {
+			'id': self.id,
+			'type': 'Feature',
+			'geometry': self.geojson,
+			'properties': self.properties
+		}
 
 ##--Child class definitions must implement makeTitle() and makeGeoJSON()
-#OPD report feed
-class policereport(service):
-	def makeTitle(self):
-		return 'A ' + self.properties['description'] + ' has occured at ' + self.properties['location'] + ' on ' + self.properties['calltime']
-	def makeGeoJSON(self):
-		return {'type': 'point' , 'coordinates': {'latitude': self.properties['location_1']['latitude'] , 'longitude': self.properties['location_1']['longitude']}}
 
-#Landmarks/POI
-class landmark(service):
+class policereport(service):
+	'''Orlando Police dispatch report feed'''
+	
 	def makeTitle(self):
-		return self.properties['name'] + ' at ' + self.properties['address']
+		ret = self.properties['reason'].capitalize()
+		ret += ' has occured at ' + self.properties['location']
+		ret += ' at ' + self.properties['when']
+		return ret
 	def makeGeoJSON(self):
-		return {'type': 'point' , 'coordinates': {'latitude': self.properties['location']['latitude'] , 'longitude': self.properties['location']['longitude']}}
+		return {
+			'type': 'point',
+			'coordinates': {
+				'latitude': self.properties['location']['coordinates'][0],
+				'longitude': self.properties['location_1']['longitude']
+			}
+		}
+
+def getTimeStamp(minAgo, strf):
+	'''Returns a strf formatted timestamp a given number of minutes prior to "now"'''
+	retTime =  datetime.now() - timedelta(minutes=minAgo)
+	return retTime.strftime(strf)
 
 #Associates each service with its object, required keys, and fetch URL
 optionDict = {
-	'police': {'obj': policereport , 'reqKeys': ['description' , 'location' , 'calltime' , 'location_1'] , 'url': 'https://brigades.opendatanetwork.com/resource/ngeg-3ist.json'},
-	'landmarks': {'obj': landmark , 'reqKeys': ['name' , 'address' , 'location'] , 'url': 'https://brigades.opendatanetwork.com/resource/hzkr-id6u.json'}
+	'police': {
+		'obj': policereport,
+		'reqKeys': ['address', 'location', 'when', 'reason'],
+		'url': ('http://brigades.opendatanetwork.com/resource/sm4t-sjt5.json?'
+				'$where=when > "{}"'.format(getTimeStamp(15, '%Y-%m-%dT%H:%M:%d')))
+	}
 }
 
 #MAIN SCRIPT
@@ -123,8 +139,10 @@ if request in optionDict:
 		serv = optionDict[request]['obj'](item)
 		#If original object contains all the required keys and their values are not null
 		if serv.checkForKeys(optionDict[request]['reqKeys']):
-			#If the new object updates and passes validation, append new object to featureList
-			if serv.update(): featureList.append(serv.export())
+			#If the new object updates and passes validation
+			if serv.update():
+				#Append new object to featureList
+				featureList.append(serv.export())
 	#Output/print the entire collection
 	output({'type': 'FeatureCollection' , 'features': featureList})
 #Else output/print error
