d71a348058aa6dcf5b562906ca090f252c1ab493
Michael duPont <michael@mdupont.com>
2018-02-01 08:32:26 -0500

    Output geom file

--- resources/voting_centers/geojson_factory.py
+++ resources/voting_centers/geojson_factory.py
@@ -1,7 +1,11 @@
 """
 geojson_factory.py - Convert ArcGIS data files into a GeoJSON output
+
+Ex: python geojson_factory.py precinct_files/May2016Precinct_region.shp
 """
 
+# stdlib
+import json
 # library
 import begin
 import shapefile
@@ -9,7 +13,7 @@ from dbfread import DBF
 
 @begin.start
 def main(input: 'Name of the files to be converted (dbf, shp)',
-         output: 'Name of the output GeoJSON file' = 'output'):
+         output: 'Name of the output GeoJSON file' = 'geometries'):
     """
     Convert ArcGIS data files into a GeoJSON output
 
@@ -17,11 +21,25 @@ def main(input: 'Name of the files to be converted (dbf, shp)',
     """
     # Remove the file ext
     input = '.'.join(input.split('.')[:-1])
+    # Import meta data and shapes
     dbf = DBF(input + '.dbf')
     shapes = shapefile.Reader(input + '.shp').shapes()
     if len(dbf) != len(shapes):
         raise Exception('Files do not have the same number of elements')
+    # Format shape files into an ID key dict of GeoJSON objects
+    features = []
     for meta, shape in zip(dbf, shapes):
-        print(meta['PRECINCT'])
-        print(shape)
-        break
+        if shape.shapeType == shapefile.POLYGONZ:
+            features.append({
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'Polygon',
+                    'coordinates': [shape.points]
+                },
+                'properties': {k.lower(): v for k, v in meta.items()}
+            })
+    # Export as a JSON file
+    json.dump({
+        'type': 'FeatureCollection',
+        'features': features
+    }, open(output + '.geojson', 'w'))
